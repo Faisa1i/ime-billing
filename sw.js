7ef73d16e885c5bdb6885f8e1938d5545a694af4
Faisa1i <46759558+Faisa1i@users.noreply.github.com>
2026-08-16 00:57:46 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="ime-billing-complete-v1";
+const CACHE="ime-billing-complete-v2";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./sw.js"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
